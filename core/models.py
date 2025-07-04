6728cd55b83814290458d57711af3453159c1e2d
Sadiya <kazisadiyairincse@gmail.com>
2025-07-04 23:14:03 +0600

    Added Class, Plan, ChatMessage, User Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from .manager import CustomUserManager
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True)
+
+    USERNAME_FIELD = 'email'    # Required for log in
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+
+class Class(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    description = models.TextField()
+
+
+class Plan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    duration = models.IntegerField(help_text="In minutes")
+    goals = models.TextField()
+
+
+class ChatMessage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    sender = models.CharField(max_length=10, choices=(('user', 'User'), ('bot', 'Bot')))
